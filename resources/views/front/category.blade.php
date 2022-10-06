1ac0a415c8514c8be5bdbc6350d24d4ae9996ea0
applepie25 <hanaulfia1612@gmail.com>
2022-10-06 10:20:41 +0700

    first commit

---
+++ resources/views/front/category.blade.php
@@ -0,0 +1,83 @@
+@extends('front/layout')
+@section('page_title','Category')
+@section('container')
+
+<!-- product category -->
+<section id="aa-product-category">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-9 col-md-9 col-sm-8 col-md-push-3">
+                <div class="aa-product-catg-content">
+                    <div class="aa-product-catg-head">
+                        <div class="aa-product-catg-head-right">
+                            <a id="grid-catg" href="#"><span class="fa fa-th"></span></a>
+                            <a id="list-catg" href="#"><span class="fa fa-list"></span></a>
+                        </div>
+                    </div>
+                    <div class="aa-product-catg-body">
+                        <ul class="aa-product-catg">
+                            <!-- start single product item -->
+
+                            @if(isset($product[0]))
+                            @foreach($product as $productArr)
+                            <li>
+                                <figure>
+                                    <a class="aa-product-img" href="{{url('product/'.$productArr->slug)}}"><img
+                                            src="{{asset('storage/media/'.$productArr->image)}}"
+                                            alt="{{$productArr->name}}"></a>
+                                    <a class="aa-add-card-btn" href="javascript:void(0)"
+                                        onclick="home_add_to_cart('{{$productArr->id}}','{{$product_attr[$productArr->id][0]->size}}','{{$product_attr[$productArr->id][0]->color}}')"><span
+                                            class="fa fa-shopping-cart"></span>Add To Cart</a>
+                                    <figcaption>
+                                        <h4 class="aa-product-title"><a
+                                                href="{{url('product/'.$productArr->slug)}}">{{$productArr->name}}</a>
+                                        </h4>
+                                        <span class="aa-product-price">Rp.
+                                            {{$product_attr[$productArr->id][0]->price}}</span><span
+                                            class="aa-product-price"><del>Rp.
+                                                {{$product_attr[$productArr->id][0]->mrp}}</del></span>
+                                    </figcaption>
+                                </figure>
+                            </li>
+                            @endforeach
+                            @else
+                            <li>
+                                <figure>
+                                    No data found
+                                    <figure>
+                            <li>
+                                @endif
+
+                        </ul>
+                        <!-- quick view modal -->
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-4 col-md-pull-9">
+                <aside class="aa-sidebar">
+                    <!-- single sidebar -->
+                    <div class="aa-sidebar-widget">
+                        <h3>Category</h3>
+                        <ul class="aa-catg-nav">
+                            <li><a href="#">Keyboard Mechanical</a></li>
+                            <li><a href="">Keycaps PBT</a></li>
+                            <li><a href="">Coiled Cable</a></li>
+                            <li><a href="">Headset Gaming</a></li>
+                        </ul>
+                    </div>
+                </aside>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- / product category -->
+
+<input type="hidden" id="qty" value="1" />
+<form id="frmAddToCart">
+    <input type="hidden" id="size_id" name="size_id" />
+    <input type="hidden" id="color_id" name="color_id" />
+    <input type="hidden" id="pqty" name="pqty" />
+    <input type="hidden" id="product_id" name="product_id" />
+    @csrf
+</form>
+@endsection
